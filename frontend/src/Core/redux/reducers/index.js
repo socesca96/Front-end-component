1857093d6cb60c87fe672b283daee642bf3be80c
Soledad <soledad.dcc.96@gmail.com>
2025-03-24 19:47:56 +0100

    implementación de los diferentes reducers

--- frontend/src/Core/redux/reducers/index.js
+++ frontend/src/Core/redux/reducers/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from "redux";
+import userReducer from "../../../Components/user/UserReducer";
+import loginReducer from "../../../Components/login/LoginReducer";
+import productReducer from "../../../Components/product/ProductReducer";
+import pagesReducer from "../../../Components/pages/PagesReducer";
 
 
-const reducers = combineReducers({})
+const reducers = combineReducers({
+    loginReducer,
+    productReducer,
+    pages: pagesReducer,
+    userReducer
+})
 
 export default reducers
