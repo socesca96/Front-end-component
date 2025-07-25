6ebf6f0cb1d6bac88364a4eeb483ed33b6fb51e5
Soledad <soledad.dcc.96@gmail.com>
2025-07-25 19:03:37 +0200

    Update login and user detail page

--- frontend/src/Components/pages/PagesAction.js
+++ frontend/src/Components/pages/PagesAction.js
@@ -1,6 +1,9 @@
 export const GO_TO_PAGE = 'GO_TO_PAGE'
 
-export const goToPageAction = (pageName) => ({
-    type: GO_TO_PAGE,
-    payload: pageName
-})
+export const goToPageAction = (pageName) => {
+    localStorage.setItem("currentPage", pageName);
+    return {
+        type: GO_TO_PAGE,
+        payload: pageName
+    }
+}
